cf29b9a83eab69a426077450a38a38507d6d88e3
AdenLe-Github <adenl56@utexas.edu>
2024-05-30 14:08:32 -0500

    Added the function of size and allowed for better UI

--- controlpanel.py
+++ controlpanel.py
@@ -2,15 +2,30 @@ from helperfunctions.myhelpers import *
 from helperfunctions.myhelperstwo import *
 from helperfunctions.codonchartimport import *
 import os
+import copy  # Importing the copy module for deep copy
 
 """This code processes the data for the output that we are interested in, the overall charge of the polypetide chain
 for the first "x" amount of nucleotides, disregarding the UTR region"""
 
 def spacer():
     #Function for visual clarity
-    print("\n\n")
+    print("\n")
+
+def switchcheck():
+    check = input("Would you like to quit or go again? (Y)/(N): ")
+    if check.lower() == "y":
+        return True
+    elif check.lower() == "n":
+        print("The code will now terminate. Thank you!")
+        return False
+    else:
+        print("Invalid input.")
+        return switchcheck()
+
+
 
 def controlpanel():
+    switch = True
     #Retrieves the mRNA file and formats the information into a dictionary where it is {gene: nt sequence}
     mrnadictionary = formattingmrna(retrieve_mrna())
 
@@ -19,27 +34,91 @@ def controlpanel():
     
     #Before the program fully starts, this function checks if the same genes are there for each of the dictionaries
     checkifgenesalign(mrnadictionary, utrdictionary)
+
     if checkifgenesalign:
+        print("Both datasets contain the same genes")
+
         #Splices out the utr region
-        maturedmrnadictionary = removeutrregions(mrnadictionary, utrdictionary)
-        spacer()
-        for key, value in maturedmrnadictionary.items():
-            #isolates the first "x" nucleotides
-            maturedmrnadictionary[key] = firstxnucleotides(value)
-            #Translates the sequence and gives the overall charge for the "x" amount of amino acids
-            maturedmrnadictionary[key] = aminoacidtranslationforcharge(maturedmrnadictionary[key])
-
-        with open("outputdata/output.txt", "w") as outputfile:
-            outputfile.write("Gene name, Transcript overall charge")
-            for key, value in maturedmrnadictionary.items():
-                outputfile.write("\n")
-                outputfile.write(f"{key}, {value}")
+        basematuredmrnadictionary = removeutrregions(mrnadictionary, utrdictionary)
         
+        spacer()
+        print("The UTR region has been spliced out")
+        spacer()
 
-    else:
-        #If the genes don't align 
-        print("The genes do not align: ERROR")
-        exit()
+        while switch == True:
+            maturedmrnadictionary = copy.deepcopy(basematuredmrnadictionary)
+            #Takes how many amino acids you would want to be analyzed
+            aminoacidcount = firstxaminoacidinput()
+            spacer()
+            #Asks for the function you would like to perform
+            branchone = input("What function would you like to perform?\n\n - Sequence the \"x\" nucleotides (A): \n - Calculate the overall charge of the sequence for \"x\" nucleotides (B): \n - Calculate the molecular weight for \"x\" nucleotides (C):")
+            
+            if branchone.lower() == "a":
+                #Sequences the transcript for the first "x" amino acids
+                for key, value in maturedmrnadictionary.items():
+                    #isolates the first "x" nucleotides
+                    maturedmrnadictionary[key] = firstxnucleotides(value, aminoacidcount)
+                    #Translates the sequence and returns a list that is given to each gene which has the first "x" amount of amino acids
+                    maturedmrnadictionary[key] = aminoacidtranslationforpolypeptide(maturedmrnadictionary[key])
+
+                #Writes the new data in another file
+                output_file_name = input("Enter the output file name (without extension): ")
+                with open(f"outputdata/{output_file_name}.txt", "w") as outputfile:
+                    emptyset = set()
+                    validationset = set("M")
+                    for key, value in maturedmrnadictionary.items():
+                        emptyset.add(value[0])
+                        outputfile.write(f"{key}, {value}")
+                        outputfile.write("\n")
+
+                    #For the code to be validated, we only want "M" in the set
+                    if emptyset == validationset:
+                        print(emptyset)
+                        print("The code is validated")
+
+                    else:
+                        print(emptyset)
+                        print("The code can't be validated, there is an error")
+                
+                switch = switchcheck()
+
+            elif branchone.lower() == "b":
+                for key, value in maturedmrnadictionary.items():
+                    #isolates the first "x" nucleotides
+                    maturedmrnadictionary[key] = firstxnucleotides(value, aminoacidcount)
+                    #Translates the sequence and gives the overall charge for the "x" amount of amino acids
+                    maturedmrnadictionary[key] = aminoacidtranslationforcharge(maturedmrnadictionary[key])
+                
+                #Writes the new data in another file
+                output_file_name = input("Enter the output file name (without extension): ")
+                with open(f"outputdata/{output_file_name}.txt", "w") as outputfile:
+                    outputfile.write("Gene name, Transcript overall charge")
+                    for key, value in maturedmrnadictionary.items():
+                        outputfile.write("\n")
+                        outputfile.write(f"{key}, {value}")
+
+                switch = switchcheck()
+
+            elif branchone.lower() == "c":
+                for key, value in maturedmrnadictionary.items():
+                    #isolates the first "x" nucleotides
+                    maturedmrnadictionary[key] = firstxnucleotides(value, aminoacidcount)
+                    #Translates the sequence and gives the overall charge for the "x" amount of amino acids
+                    maturedmrnadictionary[key] = aminoacidtranslationforsize(maturedmrnadictionary[key])
+                #Writes the new data in another file
+                output_file_name = input("Enter the output file name (without extension): ")
+                with open(f"outputdata/{output_file_name}.txt", "w") as outputfile:
+                    outputfile.write("Gene name, Transcript overall size")
+                    for key, value in maturedmrnadictionary.items():
+                        outputfile.write("\n")
+                        outputfile.write(f"{key}, {value}")
+
+                switch = switchcheck()
+
+            else:
+                #If the genes don't align 
+                print("The genes do not align: ERROR")
+                switch = switchcheck()
 
 
 
--- helperfunctions/codonchartimport.py
+++ helperfunctions/codonchartimport.py
@@ -135,3 +135,72 @@ def codonchartregular():
     'GGT': 'G'     # Glycine
     }
     return codontab
+
+def codonchartsize():
+    codontabsize = {
+            'TCA': 0,    # Serine
+            'TCC': 0,    # Serine
+            'TCG': 0,    # Serine
+            'TCT': 0,    # Serine
+            'TTC': 0,    # Phenylalanine
+            'TTT': 0,    # Phenylalanine
+            'TTA': 0,    # Leucine
+            'TTG': 0,    # Leucine
+            'TAC': 0,    # Tyrosine
+            'TAT': 0,    # Tyrosine
+            'TAA': 0,    # Stop
+            'TAG': 0,    # Stop
+            'TGC': 0,    # Cysteine
+            'TGT': 0,    # Cysteine
+            'TGA': 0,    # Stop
+            'TGG': 0,    # Tryptophan
+            'CTA': 0,    # Leucine
+            'CTC': 0,    # Leucine
+            'CTG': 0,    # Leucine
+            'CTT': 0,    # Leucine
+            'CCA': 0,    # Proline
+            'CCC': 0,    # Proline
+            'CCG': 0,    # Proline
+            'CCT': 0,    # Proline
+            'CAC': 0,    # Histidine
+            'CAT': 0,    # Histidine
+            'CAA': 0,    # Glutamine
+            'CAG': 0,    # Glutamine
+            'CGA': 0,    # Arginine
+            'CGC': 0,    # Arginine
+            'CGG': 0,    # Arginine
+            'CGT': 0,    # Arginine
+            'ATA': 0,    # Isoleucine
+            'ATC': 0,    # Isoleucine
+            'ATT': 0,    # Isoleucine
+            'ATG': 50,    # Methionine
+            'ACA': 0,    # Threonine
+            'ACC': 0,    # Threonine
+            'ACG': 0,    # Threonine
+            'ACT': 0,    # Threonine
+            'AAC': 0,    # Asparagine
+            'AAT': 0,    # Asparagine
+            'AAA': 0,    # Lysine
+            'AAG': 0,    # Lysine
+            'AGC': 0,    # Serine
+            'AGT': 0,    # Serine
+            'AGA': 100,    # Arginine
+            'AGG': 0,    # Arginine
+            'GTA': 0,    # Valine
+            'GTC': 0,    # Valine
+            'GTG': 0,    # Valine
+            'GTT': 0,    # Valine
+            'GCA': 0,    # Alanine
+            'GCC': 0,    # Alanine
+            'GCG': 0,    # Alanine
+            'GCT': 0,    # Alanine
+            'GAC': 0,   # Aspartic Acid
+            'GAT': 0,   # Aspartic Acid
+            'GAA': 0,   # Glutamic Acid
+            'GAG': 0,   # Glutamic Acid
+            'GGA': 0,    # Glycine
+            'GGC': 0,    # Glycine
+            'GGG': 0,    # Glycine
+            'GGT': 0     # Glycine
+        }
+    return codontabsize
